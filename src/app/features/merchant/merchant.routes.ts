77213d1b3ddd3143f60a9b5159734ca844418824
r_sunrise <rozasun61@gmail.com>
2025-12-31 01:31:12 +0700

    update header

--- src/app/features/merchant/merchant.routes.ts
+++ src/app/features/merchant/merchant.routes.ts
@@ -3,13 +3,33 @@ import { Routes } from '@angular/router';
 export const MERCHANT_ROUTES: Routes = [
   {
     path: '',
-    redirectTo: 'dashboard',
-    pathMatch: 'full'
-  },
-  {
-    path: 'dashboard',
     loadComponent: () =>
-      import('./pages/dashboard/dashboard')
-        .then(c => c.Dashboard)
+      import('./merchant')
+        .then(m => m.Merchant),
+    children: [
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'dashboard',
+        loadComponent: () =>
+          import('./pages/dashboard/dashboard')
+            .then(c => c.Dashboard)
+      },
+      {
+        path: 'product',
+        loadComponent: () =>
+          import('./pages/product/product')
+            .then(c => c.Product)
+      },
+      {
+        path: 'setting',
+        loadComponent: () =>
+          import('./pages/setting/setting')
+            .then(c => c.Setting)
+      }
+    ]
   }
 ];
--- src/app/features/merchant/merchant.ts
+++ src/app/features/merchant/merchant.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Dashboard } from './pages/dashboard/dashboard';
-
+import { LayoutDashboard,LucideAngularModule ,Package, ShoppingCart, Users, FileText, Settings } from 'lucide-angular';
+import { Slidebar } from '@/app/shared/components/slidebar/slidebar';
+import { KrHeader } from '@/app/shared/components/kr-header/kr-header';
+interface NavItem {
+  icon: any; 
+  label: string;
+  active?: boolean;
+  hasDropdown?: boolean;
+}
 @Component({
+  standalone: true,
   selector: 'app-merchant',
-  imports: [Dashboard,RouterOutlet],
+  imports: [RouterOutlet  ,LucideAngularModule, KrHeader , Slidebar ],
   templateUrl: './merchant.html',
   styleUrl: './merchant.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class Merchant {
-  
+   navItems: NavItem[] = [
+    { icon: LayoutDashboard, label: 'ផ្ទាំងគ្រប់គ្រង', active: true },
+    { icon: Package, label: 'ផលិតផល', hasDropdown: true },
+    { icon: ShoppingCart, label: 'ការបញ្ជាទិញ', hasDropdown: true },
+    { icon: Users, label: 'អតិថិជន' },
+    { icon: FileText, label: 'របាយការណ៍', hasDropdown: true },
+    { icon: Settings, label: 'ការកំណត់' , hasDropdown : false},
+  ];
+    user = {
+    name: 'សុខ វណ្ណា',
+    role: 'អ្នកគ្រប់គ្រង',
+    avatar: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?w=100&h=100&fit=crop&crop=face'
+  };
+
+  title = "ផ្ទាំងគ្រប់គ្រង";
+  subtitle = "សូមស្វាគមន៍មកកាន់ Krama Dashboard";
+  date = "០៨ ធ្នូ ២០២៤"
+  notificationCount = 10;
 }
